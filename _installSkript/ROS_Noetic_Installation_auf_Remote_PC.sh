d28a73a9389ad9d5f00129cdc67023e63f824f8d
GeraldHebinck <43569784+GeraldHebinck@users.noreply.github.com>
2020-06-30 11:21:16 +0200

    Anpassung Texte und Farben

--- _installSkript/ROS_Noetic_Installation_auf_Remote_PC.sh
+++ _installSkript/ROS_Noetic_Installation_auf_Remote_PC.sh
@@ -4,41 +4,38 @@
 # EMR SS2020 Hebinck, Heid
 
 #!/bin/bash
+echo -e "\033[1;92m ---------- Skript zur Installation von ROS Noetic auf Ubuntu 20.04  ------------ \033[0m "
 
-echo -e "\033[34m ---------- ROS installieren und Workspace einrichten  ------------ \033[0m "
-
+echo -e "\033[42m ---------- Systemupdates werden ausgefuehrt - Passwort erforderlich  ------------ \033[0m "
 sudo apt update
 sudo apt-get dist-upgrade -y
 sudo apt install -y git 
  
-echo -e "\033[34m ---------- Installiere ROS-Noetic  http://wiki.ros.org/noetic/Installation/Ubuntu  ------------ \033[0m "
+echo -e "\033[42m ---------- Installiere ROS-Noetic  http://wiki.ros.org/noetic/Installation/Ubuntu  ------------ \033[0m "
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
 sudo apt install -y ros-noetic-desktop-full
 
-echo -e "\033[34m ---------- Erstelle catkin_ws  ------------ \033[0m "
+echo -e "\033[42m ---------- Erstelle catkin_ws  ------------ \033[0m "
 mkdir -p ~/catkin_ws/src
 mkdir -p ~/catkin_ws/devel
 touch ~/catkin_ws/devel/setup.bash
 
-echo -e "\033[34m ---------- Konfiguriere .bashrc ------------ \033[0m "
-
+echo -e "\033[42m ---------- Konfiguriere .bashrc ------------ \033[0m "
 echo "export LC_NUMERIC="en_US.UTF-8"" >> ~/.bashrc
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
 echo "export ROS_PACKAGE_PATH=~/catkin_ws/src:/opt/ros/noetic/share" >> ~/.bashrc
 echo "export ROS_MASTER_URI=http://localhost:11311" >> ~/.bashrc
 echo "export ROS_HOSTNAME=127.0.0.1" >> ~/.bashrc
+source ~/.bashrc # Damit source funktioniert, muss das Skript mit Bash und nicht mit SH ausgefuehrt werden
 
-# anscheinend funktioniert "source" nicht korrekt im shellskript
-# source ~/.bashrc
-
-echo -e "\033[34m ---------- Dependencies for building packages ------------ \033[0m "
+echo -e "\033[42m ---------- Installiere Dependencies und Buildtools ------------ \033[0m "
 sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
 sudo rosdep init
 rosdep update
 
-echo -e "\033[34m ---------- Installation finished ------------ \033[0m "
+echo -e "\033[42m ---------- Installation beendet ------------ \033[0m "
 
 
