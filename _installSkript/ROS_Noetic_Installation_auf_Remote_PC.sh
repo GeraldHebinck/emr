44baf8d8ba9fd3379a927e777c4d82f8a30e78e2
GeraldHebinck <43569784+GeraldHebinck@users.noreply.github.com>
2020-07-03 10:29:53 +0200

    updated .bashrc config

--- _installSkript/ROS_Noetic_Installation_auf_Remote_PC.sh
+++ _installSkript/ROS_Noetic_Installation_auf_Remote_PC.sh
@@ -23,12 +23,19 @@ mkdir -p ~/catkin_ws/devel
 touch ~/catkin_ws/devel/setup.bash
 
 echo -e "\033[42m ---------- Konfiguriere .bashrc ------------ \033[0m "
+echo "### ROS - Umgebungsvariablen ###" >> ~/.bashrc
 echo "export LC_NUMERIC="en_US.UTF-8"" >> ~/.bashrc
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
 echo "export ROS_PACKAGE_PATH=~/catkin_ws/src:/opt/ros/noetic/share" >> ~/.bashrc
+echo "## Damit eine Netzwerkkommunikation möglich ist, IP-Adressen einstellen! ##" >> ~/.bashrc
+echo "## IP des PCs auf dem der Master laeuft ## " >> .bashrc
 echo "export ROS_MASTER_URI=http://localhost:11311" >> ~/.bashrc
+echo "# export ROS_MASTER_URI=http://192.168.0.40:11311" >> ~/.bashrc
+echo "## IP dieses Rechners " >> .bashrc
 echo "export ROS_HOSTNAME=127.0.0.1" >> ~/.bashrc
+echo "#export ROS_HOSTNAME=http://192.168.0.40:11311" >> ~/.bashrc
+echo "### Ende ROS - Umgebungsvariablen ###" >> ~/.bashrc
 source ~/.bashrc # Damit source funktioniert, muss das Skript mit Bash und nicht mit SH ausgefuehrt werden
 
 echo -e "\033[42m ---------- Installiere Dependencies und Buildtools ------------ \033[0m "
